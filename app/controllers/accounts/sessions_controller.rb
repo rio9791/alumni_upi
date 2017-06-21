0836f41776b635ab3c5dcb29899b44bfae29230f
Rio Dermawan <rio.dermawan57@gmail.com>
2017-06-21 17:14:28 +0700

    Login and logout session

--- app/controllers/accounts/sessions_controller.rb
+++ app/controllers/accounts/sessions_controller.rb
@@ -2,19 +2,31 @@ class Accounts::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    account = Account.find_by_email(params[:email])
+    if account && account.authenticate(params[:password])
+      session[:account] = account.id
+      sign_in account
+      unless account.has_role?(:admin)
+        redirect_to root_path
+      else
+        redirect_to admin_dashboard_index_path
+      end
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # protected
 
