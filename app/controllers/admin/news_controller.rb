49b5d6d79466f8931ad6e4a0c6f0519f7d8d706d
Rio Dermawan <rio.dermawan57@gmail.com>
2017-07-12 07:35:55 +0700

    Fix error on page admin jobs

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -64,7 +64,7 @@ class Admin::NewsController < Admin::ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_news
-      @news = News.find(params[:id])
+      @news = News.find_by_slug(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
