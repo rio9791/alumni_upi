b07fbe0f4764bfa2d7e15bc149be88bf6d7c5149
Rio Dermawan <rio.dermawan57@gmail.com>
2017-07-15 13:36:22 +0700

    Fix error slug on admin jobs

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -64,7 +64,7 @@ class Admin::JobsController < Admin::ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_job
-      @job = JobVacancy.find(params[:id])
+      @job = JobVacancy.find_by_slug(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
