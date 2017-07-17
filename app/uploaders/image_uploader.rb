0aa3eb84b53d1d3f4493a45c09c331b632886bdc
Rio Dermawan <rio.dermawan57@gmail.com>
2017-07-18 02:13:36 +0700

    Update image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,6 +34,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [50, 50]
   end
 
+  version :frontend do
+    process resize_to_fit: [640, 400]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
